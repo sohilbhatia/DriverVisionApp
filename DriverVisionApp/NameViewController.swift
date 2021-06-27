e46a2dbaf087d195f3d30fb5ec78b4ebe1b7766f
Sohil Bhatia <bhatiasohil@hotmail.com>
2021-06-27 14:29:05 -0700

    user defaults for get started

--- DriverVisionApp/NameViewController.swift
+++ DriverVisionApp/NameViewController.swift
@@ -43,6 +43,7 @@ class NameViewController: UIViewController {
     @IBAction func nxtClick(_ sender: Any) {
         name = nameField.text!
         print(defaults.value(forKey: "code")!)
+        UserDefaults.standard.set(true, forKey: "isLogged")
         let g_vc = self.storyboard?.instantiateViewController(identifier: "map_vc") as! MapViewController
         self.view.window?.rootViewController = g_vc
         self.view.window?.makeKeyAndVisible()
